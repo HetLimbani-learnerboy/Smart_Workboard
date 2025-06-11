dcee2a510f4b9e473652b261318076317985d935
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-06-11 10:21:27 +0530

    project working

---
+++ src/Admin/Addemployee.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Addemployee() {
+    return (
+        <div>
+            <h2>Add Employee</h2>
+            {/* Your form or content goes here */}
+        </div>
+    );
+}
+
+export default Addemployee;
